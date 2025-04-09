d240537a5cbd9dd46028e8d02a0e430a67208600
Isaac <joshuaisaac@karunya.edu.in>
2025-04-09 14:10:18 +0530

    query update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,4 @@ CREATE TABLE course (
 INSERT INTO course (id, name, author) VALUES (1, 'Spring Boot', 'John Doe');
 INSERT INTO course (id, name, author) VALUES (2, 'AWS', 'Joshua');
 INSERT INTO Course (id, name, author) VALUES (3, 'Spring', 'John');
+INSERT INTO Course (id, name, author) VALUES (5, 'boot', 'isaac');
